673c0bf24e9ce04b63851156b3c3cfc11fe09746
Abhishek Sharma <abhioncbr@yahoo.com>
2020-04-13 18:24:25 -0400

    Updated resource group terraform template. (#2)
    * Added subscription id & active directory features for rg.
    
    * updated resource-group template.
    
    Co-authored-by: abhioncbr <abhioncbr@Abhisheks-MacBook-Pro.local>

--- azure/resource-group/variables.tf
+++ azure/resource-group/variables.tf
@@ -1,24 +1,40 @@
-# azurerm provider variables.
-variable "subscription_id" {
-    type = string
-    description = "Subscription ID of a azure"
+# =========================
+# active directory app auth
+variable "app_auth" {
+    type = bool
+    description = "[Required]: AD app based authentication for resource creation."
 }
 
+# AD app auth variables.
 variable "tenant_id" {
     type = string
-    description = "Application Id of azure in azure active directory."
+    description = "[Required if app_auth=true]: Application Id of azure in azure active directory."
 }
 
 variable "client_id" {
     type = string
-    description = "Client Id for an Application ID."
+    description = "[Required if app_auth=true]: Client Id for an Application ID."
 }
 
 variable "client_secret" {
     type = string
-    description = "Client Secret of a Client Id for an Application ID."
+    description = "[Required if app_auth=true]: Client Secret of a Client Id for an Application ID."
 }
 
+# =========================
+# active directory app auth
+variable "subscription_provided" {
+    type = bool
+    description = "[Required]: subscription id provided for specific subsription."
+}
+
+# azurerm provider variables.
+variable "subscription_id" {
+    type = string
+    description = "[Required if subscription_provided=true]: Subscription ID of a azure."
+}
+
+# =========================
 # resource group variables.
 variable "resource_group_name" {
     type = string
