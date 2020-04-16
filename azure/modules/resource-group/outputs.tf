5c701818e0f4674b6c4aec6ea9bca2950b5a4a5a
Abhishek Sharma <abhioncbr@yahoo.com>
2020-04-16 16:48:25 -0400

    updated structure and resources.

---
+++ azure/modules/resource-group/outputs.tf
@@ -0,0 +1,9 @@
+output "resource_group_name" {
+  value           = "${azurerm_resource_group.resource-group.name}"
+  description     = "Terraform created azure resource group name."
+}
+
+output "resource_group_id" {
+  value           = "${azurerm_resource_group.resource-group.id}"
+  description     = "Terraform created azure resource group id."
+}
