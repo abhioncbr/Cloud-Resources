38b05e31851c81559cf859198049f67ca2d7cd7c
Abhishek Sharma <abhioncbr@yahoo.com>
2020-04-16 16:44:12 -0400

    updated structure and resources. (#3)

---
+++ azure/examples/resource-group/rg-local-state.tf
@@ -0,0 +1,27 @@
+# Main resource group for the demonstration.
+
+variable "tags"                     {}
+variable "location"                 {}
+variable "app_auth"                 {}
+variable "tenant_id"                {}
+variable "client_id"                {}
+variable "client_secret"            {}
+variable "subscription_id"          {}
+variable "subscription_provided"    {}
+variable "resource_group_name"      {}
+
+provider "azurerm" {
+    features          {}
+    version           = "=2.0.0"
+    tenant_id         = var.app_auth ? var.tenant_id : null
+    client_id         = var.app_auth ? var.client_id : null
+    client_secret     = var.app_auth ? var.client_secret : null
+    subscription_id   = var.subscription_provided ? var.subscription_id : null
+}
+
+module "local-state-resource-group" {
+    tags                        = "${var.tags}"
+    location                    = "${var.location}"
+    resource_group_name         = "${var.resource_group_name}"
+    source                      = "../../modules/resource-group"
+}
